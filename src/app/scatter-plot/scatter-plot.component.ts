a1ab98845dd24fbf1a7660b04a2767b181bf95c0
Robert Decker <robert.decker@dataunveil.com>
2025-02-19 02:57:24 -0500

    Working switching of themes

--- src/app/scatter-plot/scatter-plot.component.ts
+++ src/app/scatter-plot/scatter-plot.component.ts
@@ -13,19 +13,77 @@ import { NumberSymbol } from '@angular/common';
 })
 export class ScatterPlotComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
     this.makeChart("anomalies");
   }
 
+  upperBound = 2;
+  lowerBound = -2;
+
+
   selectedValue: string = 'anomalies'; // Default value
   onValueChange() {
-    this.makeChart(this.selectedValue);
+    if (this.selectedValue === "anomalies"){
+      d3.selectAll("circle")
+      // Remove all classes
+      .attr("class", null)
+      // Remove all styles
+      .attr("style", null)
+      .attr("class", (d:any) => {
+        if (d.y > this.upperBound || d.y < this.lowerBound){
+            return "anamoly";
+        }
+        else{
+            return "normal";
+        }
+      });
+    }
+    else{
+      d3.selectAll("circle")
+      // Remove all classes
+      .attr("class", null)
+      // Remove all styles
+      .attr("style", null)
+      .attr("style", (d:any) => {
+        if (this.selectedValue === "sources"){
+          return `fill: ${this.known_source_colors[d.source_name] || "gray"}`;
+        }
+        if (this.selectedValue === "kpi"){
+          return `fill: ${this.known_kpi_colors[d.kpi_name] || "gray"}`;
+        }
+        else{
+          return ``
+        }
+      })
+    }
+    
   }
 
+  known_source_colors: Record<string, string> = {}; // Maps source_name to color
+  known_kpi_colors: Record<string, string> = {}; // Maps kpi_name to color
+
+  darkColors: string[] = ["#E74028", "#2CB239", "#25D3EB", "#FFB039", "#1566DE", "#7848FF", "#7AD045", "#CB04DC", "#FA195C", "#9E0000", "#EACD37", "#DD630B", "#BCD03F", "#14BDA9", "#8A05F3", "#D9476A", "#2DADB6", "#E468C2", "#199CD4", "#0934CC",
+          "#e74c3c", "#3498db", "#2ecc71", "#f1c40f", "#9b59b6",
+          "#1abc9c", "#34495e", "#e67e22", "#16a085", "#d35400",
+          "#2980b9", "#8e44ad", "#c0392b", "#27ae60", "#f39c12",
+          "#7f8c8d", "#e74c3c", "#3498db", "#2ecc71", "#f1c40f",
+          "#9b59b6", "#1abc9c", "#34495e", "#e67e22", "#16a085",
+          "#d35400", "#2980b9", "#8e44ad", "#c0392b", "#27ae60",
+          "#f39c12", "#7f8c8d", "#e74c3c", "#3498db", "#2ecc71",
+          "#f1c40f", "#9b59b6", "#1abc9c", "#34495e", "#e67e22",
+          "#16a085", "#d35400", "#2980b9", "#8e44ad", "#c0392b",
+          "#27ae60", "#f39c12", "#7f8c8d"];
+
+    
+
+
   makeChart(theme: string){
-    console.log(DQM_Data.length)
+    this.known_source_colors = {} as Record<string, string>;
+this.known_kpi_colors = {} as Record<string, string>;
 
     // Need to fetch our data
     
@@ -40,19 +98,30 @@ export class ScatterPlotComponent implements OnInit {
     interface dqm_scatter_data{
       x: number;
       y: number;
+      source_name: string;
+      kpi_name: string;
     }
 
     const data: dqm_scatter_data[] = DQM_Data.map((d:any) => ({
         x: d.volume,
         y: d.kpi_zscore,
+        source_name: d.source_name,
+        kpi_name: d.kpi_name
     }));
 
+    data.forEach((d:any, index:number) => {
+      // If kpi color not set then set the color
+      if (!this.known_source_colors[d.source_name]) {
+        this.known_source_colors[d.source_name] = this.darkColors[index % this.darkColors.length];
+      }
+      // If source color not set then set the color
+      if (!this.known_kpi_colors[d.kpi_name]) {
+        this.known_kpi_colors[d.kpi_name] = this.darkColors[(index) % this.darkColors.length]; // Using a different index for KPI colors
+      }
+    });
     
     const xScale = d3.scaleLinear().domain([0, 10000]).range([margin.left, width - margin.right]);
     const yScale = d3.scaleLinear().domain([-5, 5]).range([height - margin.bottom, margin.top]);
-
-    const upperBound = 2;
-    const lowerBound = -2;
 
     const xAxis = d3.axisBottom(xScale)
         .tickSize(-(height - margin.bottom - margin.top))
@@ -111,7 +180,7 @@ export class ScatterPlotComponent implements OnInit {
         .data(data)
         .enter().append("circle")
         .attr("class", (d) => {
-            if (d.y > upperBound || d.y < lowerBound){
+            if (d.y > this.upperBound || d.y < this.lowerBound){
                 return "anamoly";
             }
             else{
@@ -131,7 +200,7 @@ export class ScatterPlotComponent implements OnInit {
     function showToolTip(event:any, d:any){
       console.log(d);
         tooltip
-            .html(`Volume: ${d.x}<br>Z-Score: ${d.y} `)
+            .html(`<b>Source: ${d.source_name}</b><br>KPI Name: ${d.kpi_name}<br>Volume: ${d.x}<br>Z-Score: ${d.y}`)
             .style("left", (event.pageX)+ 10 + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
             .style("top", (event.pageY)+ 10 + "px")
     }
